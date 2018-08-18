f39bb155df11978ecadec83410c33e23c897873a
jeffreysjunk <42415057+jeffreysjunk@users.noreply.github.com>
2018-08-17 23:45:47 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ http.createServer(function(request, response) {
     console.log(request.url);
 
     new https.request({
-      hostname: 'https://www.pornhub.com/',
+      host: 'www.pornhub.com/',
       headers: {
-        'Host': 'https://www.pornhub.com/',
+        'Host': 'www.pornhub.com/',
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
         'Accept-Language': 'en-US,en;q=0.5',
